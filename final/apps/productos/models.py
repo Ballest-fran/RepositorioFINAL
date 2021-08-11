9dc5d31f95b62bae886358ed7a5f771251dabadb
Nicolas <nicotortosa@gmail.com>
2021-08-11 16:05:19 -0300

    Creado Genericos

--- final/apps/productos/models.py
+++ final/apps/productos/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 from apps.core.models import TimeModel
-# Create your models here.
+
+from apps.genericos.models import SubRubro
 
 class Producto(TimeModel):
 	nombre = models.CharField(max_length = 30)
 	precio = models.DecimalField(max_digits=8, decimal_places=2)
 	stock = models.IntegerField(default = 0)
-	
+	subrubro = models.ForeignKey(SubRubro, on_delete = models.SET_NULL , null = True)
 
